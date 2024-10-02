f1cfefc7b8891bea951249e7ea590127ea286638
iavcoelho <iavcoelho@ua.pt>
2024-10-02 23:26:23 +0100

    Added comments to explain my thought process

--- Memifier/utils/CommandBuilder.ts
+++ Memifier/utils/CommandBuilder.ts
@@ -4,9 +4,16 @@ export class CommandBuilder {
   /**
    * A builder class that provides methods to incrementally
    * build the arguments that will be passed to FFMPEG.
-   * Finally building the object will return the execute session from FFMPEG
+   * Each builder method returns the builder itself so we can chain the methods.
+   * Finally building the object will return the execute session promise from FFMPEG
    */
 
+  // The reason we are storing the arguments in individual strings instead of a single string to where we
+  // concatenate the arguments is because this way we can know for sure that no single argument is repeated
+  // i.e. we cannot add two input files, or two output files.
+  // This way, we do not have to worry so much in the rest of the codebase about that issue,
+  // and we know for sure  that only the last call per argument is used
+  // (So for example, if we setInput twice, only the last call will be stored)
   input: string;
   output: string;
   time_start: string;
@@ -45,7 +52,12 @@ export class CommandBuilder {
   }
 
   setWidth(width: string): CommandBuilder {
-    this.width = `-vf "fps=${this.fps},scale=${width}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"`;
+    // TODO: I do not know what happens if we call this with fps not set,
+    // nor do I know if we can set the scale without setting the FPS.
+    // I just assume that it will use the original FPS in case it is not overriden
+    this.width = this.fps
+      ? `-vf "fps=${this.fps},scale=${width}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"`
+      : `-vf "scale=${width}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"`;
     return this;
   }
 
@@ -63,6 +75,10 @@ export class CommandBuilder {
       this.output,
     ];
 
+    // From what I know, executeWithArguments receives a string[], where each string is a single argument.
+    // It will then return a Promise of an FFMPEG session, which is what will actually be running FFMPEG.
+    // It is the session that the rest of the program will use to see if the execution was successful or not
+    // Therefore, we can just directly return the Promise
     return FFmpegKit.executeWithArgumentsAsync(argumentList);
   }
 }
